ff5fef0904b63fd4f3ba59420cfa7918c5822fd2
Zachary Pelletier <zpell057@uottawa.ca>
2024-08-06 18:31:19 -0400

    added 3d modeling projects, end of personnal projects section

--- scripts.js
+++ scripts.js
@@ -16,7 +16,10 @@ const en={
     "appDesc":"Arithmetic practice Android app developped in Java using Android Studio",
     "chartsDesc1":"Series of python programs made to edit a video compilation of all #1 songs on the canadian charts in a given decade. The data of what songs were #1 during what time was scraped off of wikipedia using ",
     "chartsDesc2":"to then be searched and downloaded from Youtube using google cloud's Youtube Data API v3 and ",
-    "chartsDesc3":"The final video was then created by editing 10 second clips from each video and adding text showing info about the song with"
+    "chartsDesc3":"The final video was then created by editing 10 second clips from each video and adding text showing info about the song with",
+    "modelDesc":"Various 3D modeling projects done in Fusion 360 and onshape.",
+    "appleMouse":"Old Apple computer mouse converted to optical and USB to be used on modern computers using the internals of a modern mouse."
+
 }
 const fr={
     "Studies":"Étudiant en B.Sc.A. Génie électrique et B.Sc. Technologie de l'informatique",
@@ -33,7 +36,10 @@ const fr={
     "appDesc":"Application Andoid de pratique arithmétique réalisée sur Android Studio",
     "chartsDesc1":"Série de programmes python qui réalisent une compilation de tous les chansons #1 aux charts canadiennes pour une décennie données. L'information sur quelles chansons étaient #1 a été obtenue sur Wikipedia avec",
     "chartsDesc2":"pour ensuite rechercher et télécharger ces chansons sur youtube avec l'API Youtube Data API v3 de google cloud et",
-    "chartsDesc3":". La vidéo finale a été créée en montant des segments de 10 secondes de chaque vidéo et en ajoutant du texte avec l'information sur la chanson à l'aide de "
+    "chartsDesc3":". La vidéo finale a été créée en montant des segments de 10 secondes de chaque vidéo et en ajoutant du texte avec l'information sur la chanson à l'aide de ",
+    "modelDesc":"Multiples projets de modélisation 3D avec Fusion 360 et onshape.",
+    "appleMouse":"Ancienne souris d'ordinateur Apple modernisée avec une souris optique moderne pour être utilisée par USB."
+
 }
 
 function switchLanguage(){
